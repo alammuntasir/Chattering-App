a8c33293bc72b6a8be7ec66eb097bfd209b26eb2
alammuntasir <alammuntasir01@gmail.com>
2025-08-25 23:20:44 +0600

    Signup page validation Done

--- src/assets/pages/Signup.jsx
+++ src/assets/pages/Signup.jsx
@@ -1,77 +1,133 @@
-import React from 'react'
-import { Link } from 'react-router'
-
+import React, { useState } from "react";
+import { Link } from "react-router";
+import toast, { Toaster } from 'react-hot-toast';
 const Signup = () => {
-  return (
-     <section className="bg-gray-50 dark:bg-green-900 font-Candal">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <a
-      href="#"
-      className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-    >
-     
-      Chattering-App
-    </a>
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Sign Up to your account
-        </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@company.com"
-              required=""
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required=""
-            />
-          </div>
-          
-          <button
-            type="submit"
-            className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            Sign Up
-          </button>
-          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Have an account yet?{" "}
-            <Link
-              to={"/login"}
-              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-            >
-              Sign In
-            </Link>
-          </p>
-        </form>
-      </div>
-    </div>
-  </div>
-</section>
-  )
-}
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default Signup
+
+  const handleName = (e) => {
+    setUserInfo((prev) => {
+      return { ...prev, name: e.target.value };
+    });
+  };
+  const handleEmail = (e) => {
+    setUserInfo((prev) => {
+      return { ...prev, email: e.target.value };
+    });
+  };
+  const handlepassword = (e) => {
+    setUserInfo((prev) => {
+      return { ...prev, password: e.target.value };
+    });
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    
+    if (!userInfo.name || !userInfo.email ||!userInfo.password) {
+      toast.error("all fill  are required");
+    } else
+    if (!/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/.test(userInfo.email)){
+      toast.error("Invalid Email Address");
+    } else {
+      console.log(userInfo)
+    }
+      
+  } 
+
+  return (
+    <section className="bg-gray-50 dark:bg-green-900 font-Candal">
+       <Toaster />
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          Chattering-App
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign Up to your account
+            </h1>
+            <form onSubmit={handleSignup} className="space-y-4 md:space-y-6">
+              <div>
+                <label
+                  htmlFor="Name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Name
+                </label>
+                <input
+                  onChange={handleName}
+                  type="text"
+                  name="text"
+                  id="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter Your Name...."
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                onChange={handleEmail}
+                  type="text"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                onChange={handlepassword}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign Up
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Have an account yet?{" "}
+                <Link
+                  to={"/login"}
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign In
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwKPhSlvBOTDYtZWj_dy8aoLhGWIOyS-k",
+  authDomain: "chatteringapp.firebaseapp.com",
+  projectId: "chatteringapp",
+  storageBucket: "chatteringapp.firebasestorage.app",
+  messagingSenderId: "435774590054",
+  appId: "1:435774590054:web:2caaca8d5ced528f00b1a7",
+};
+
+const app = initializeApp(firebaseConfig);
